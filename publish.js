ebd6b7f4654802b59ebb7af0e72353aa23643da4
Caleb Davis <calebistaken@gmail.com>
2025-09-18 21:14:14 -0700

    add hyphenation to headings
    So words like "Unphotographable" break nicely on mobile

--- publish.js
+++ publish.js
@@ -6,7 +6,7 @@ let commit = ''; //'c91addc261a733b497333a65a1241cdf119d6464'; // at least the f
 let v      = ''; // Math.floor(Math.random() * 1000); // cache-bust with random version #
 
 commit = '' && '@'   + commit;
-v =      '' && '?v=' + v;
+v =      '' && '?v=' + v);
 
 
 /* ==============================================================
@@ -14,35 +14,35 @@ v =      '' && '?v=' + v;
    ============================================================== */
 
 // add classes to the body for minimal compatability with stylesettings
-import "./js/style-settings.js";
+import(repo + commit + "/js/style-settings.js" + v);
 
 // wrap images with alt-text in figures with figcaps 
-import "./js/photo-captions.js";
+import(repo + commit + "/js/photo-captions.js" + v);
 
 // Auto switch light/dark theme with the browser
-import "./js/auto-light-dark-switching.js";
+import(repo + commit + "/js/auto-light-dark-switching.js" + v);
 
 // Sanitize display text of links and titles by removing the leading YYYYMMDDx date string.
-import "./js/sanitize-filenames.js";
+import(repo + commit + "/js/sanitize-filenames.js" + v);
 
 // Fullscreen Lightbox image gallary for pictures on click
-import "./js/image-lightbox.js";
+import(repo + commit + "/js/image-lightbox.js" + v);
 
 // Add `[Prev] | [Next]` buttons to each note
-import "./js/next-previous-story.js";
+import(repo + commit + "/js/next-previous-story.js" + v);
 
 // Make h1.page-header look like normal h1
-import "./js/h1-page-header.js";
+import(repo + commit + "/js/h1-page-header.js" + v);
 
 // Add a location map to the right sidebar
-import "./js/insert-maps.js";
+import(repo + commit + "/js/insert-maps.js" + v);
 
 // Add a dates abov the H1
-import "./js/insert-dates.js";
+import(repo + commit + "/js/insert-dates.js" + v);
 
 // Add Disqus comments to the bottom of the story
 window.DISQUS_SHORTNAME = 'adventure-stories';
-import "./js/disqus.js";
+import(repo + commit + "/js/disqus.js" + v);
 
 
 
